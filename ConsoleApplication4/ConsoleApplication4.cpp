56fafaaa3b0c8c3832f2b42cfdaac0af42e45d8c
RenataSharafutdinova <renatkasharafutdinova@gmail.com>
2024-07-11 21:34:42 +0300

    макросы

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -1,78 +1,69 @@
 ﻿#include <iostream>
 #include <cmath>
-#include <ctime>
-
+#include <math.h>
 using namespace std;
 
+//нахождение максимума
+//#define MAX(a, b) a >= b ? a : b
+//int main() {
+//	int a, b, max;
+//	cin >> a; cin >> b;
+//	max= MAX(a, b);
+//	cout << max;
+//}
+
+
+//возыедение числа в степень
+//#define power(a, n) { \
+//    double rez = 1; \
+//    for (size_t i = 0; i < (n); ++i) { \
+//        rez *= (a); \
+//    } \
+//    cout<<rez; \
+//}
+//int main()
+//{
+//    double a, n;
+//    cin >> a;
+//    cin >> n;
+//    power(a, n);
+//   
+//    return 0;
+//}
+
+//среднее арифметическое двух чисел
+//#define average(n1,n2) (((n1)+(n2))/2)
+//int main() {
+//	double n1, n2;
+//	cin >> n1; cin >> n2;
+//	cout << average(n1,n2);
+//}
+
+//модуль числа
+//#define module(a) fabs(a)
+//int main() {
+//	double a;
+//	cin >> a;
+//	cout << module(a);
+//}
+
+//перевод градусов Цельсия в Фаренгейты
+//#define degreesC_F(n) ((n)*9)/5+32
+//int main() {
+//	int n;
+//	cin >> n;
+//	cout << degreesC_F(n);
+//}
+
+//сумма квадратов двух чисел
+#define sum_of_squares(n1, n2) ((n1)*(n1))+((n2)*(n2))
 int main() {
-    setlocale(LC_ALL, "Russian");
-    double a[8];
-    double s_minus = 0;
-    cout << "Введите 8 чисел:\n";
-
-    for (int i = 0; i < 8; i++) {
-        cin >> a[i];
-        if (a[i] < 0) {
-            s_minus += a[i];
-        }
-
-    }
-    int minI, maxI;
-    double min = a[0], max = a[0], pr1 = 1, pr2 = 1;
-    for (int i = 0; i < 8; i++) {
-        if (a[i] > max) {
-            max = a[i];
-            maxI = i;
-
-        }
-        else if (a[i] < min) {
-
-            min = a[i];
-            minI = i;
-
-        }
-
-        if (i % 2 == 0) {
-            pr2 *= a[i];
-        }
-    }
-
-
-    if (minI < maxI) {
-
-        for (; minI + 1 < maxI; minI++) {
-
-            pr1 *= a[minI + 1];
-        }
-    }
-    else {
-        for (; maxI + 1 < minI; maxI++) {
-
-            pr1 *= a[maxI + 1];
-        }
-    }
-
-    double s = 0;
-    int l1, l2;
-    for (int i = 0; i < 8; i++) {
-        if (a[i] < 0) {
-            l1 = i;
-            break;
-        }
-    }
-    for (int i = 7; i >= 0; i--) {
-        if (a[i] < 0) {
-            l2 = i;
-            break;
-        }
-    }
-    for (; l1 + 1 < l2; l1++) {
-        s += a[l1 + 1];
-    }
-
-    cout << "Сумма всех отрицательных чисел " << s_minus << endl;
-    cout << "Произведение чисел между мин и макс " << pr1 << endl;
-    cout << "Произведение чисел с четными индексами " << pr2 << endl;
-    cout << "Сумма чисел между первым и вторым отрицательным числом "<< s << endl;
-
+	int n1, n2;
+	cin >> n1; cin >> n2;
+	cout << sum_of_squares(n1, n2);
 }
+
+
+
+
+
