48b98f8b654bc3b606e071561e8c810d1b722aca
RenataSharafutdinova <renatkasharafutdinova@gmail.com>
2024-08-28 00:28:05 +0300

    Lift is complited

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -1,69 +1,49 @@
 ﻿#include <iostream>
-#include <cmath>
-#include <math.h>
+#include <string>
 using namespace std;
 
-//нахождение максимума
-//#define MAX(a, b) a >= b ? a : b
-//int main() {
-//	int a, b, max;
-//	cin >> a; cin >> b;
-//	max= MAX(a, b);
-//	cout << max;
-//}
+class Lift {
+private:
+	int maxFloor;
+	int minFloor;
+	bool OnOff;
+	int currentFloor;
 
+public:
 
-//возыедение числа в степень
-//#define power(a, n) { \
-//    double rez = 1; \
-//    for (size_t i = 0; i < (n); ++i) { \
-//        rez *= (a); \
-//    } \
-//    cout<<rez; \
-//}
-//int main()
-//{
-//    double a, n;
-//    cin >> a;
-//    cin >> n;
-//    power(a, n);
-//   
-//    return 0;
-//}
+	Lift(int maxFloorValue, int minFloorValue,bool OnOffValue, int currentFloorValue) {
 
-//среднее арифметическое двух чисел
-//#define average(n1,n2) (((n1)+(n2))/2)
-//int main() {
-//	double n1, n2;
-//	cin >> n1; cin >> n2;
-//	cout << average(n1,n2);
-//}
+		maxFloor = maxFloorValue;
+		minFloor = minFloorValue;
+		OnOff = OnOffValue;
+		currentFloor = currentFloorValue;
+	}
+	void liftIsWorking() {
 
-//модуль числа
-//#define module(a) fabs(a)
-//int main() {
-//	double a;
-//	cin >> a;
-//	cout << module(a);
-//}
+		if (OnOff) {
 
-//перевод градусов Цельсия в Фаренгейты
-//#define degreesC_F(n) ((n)*9)/5+32
-//int main() {
-//	int n;
-//	cin >> n;
-//	cout << degreesC_F(n);
-//}
+			cout << "Lift is working";
+		}
+		else {
+			cout << "Lift is not working";
+		}
+	}
+	void Print() {
 
-//сумма квадратов двух чисел
-#define sum_of_squares(n1, n2) ((n1)*(n1))+((n2)*(n2))
+		cout << "Maximum floor: " << maxFloor << "\nMinimum flooor: " << minFloor << endl;
+		liftIsWorking();
+		cout<< "\nCurrent floor: " << currentFloor << endl;
+		
+	}
+
+};
 int main() {
-	int n1, n2;
-	cin >> n1; cin >> n2;
-	cout << sum_of_squares(n1, n2);
+
+	Lift theFirstLift(12, 1, true, 3);
+	theFirstLift.Print();
+	
+	cout << endl<<"---------------------------"<<endl;
+
+	Lift theSecondLift(9, 1, false, 5);
+	theSecondLift.Print();
 }
-
-
-
-
-
